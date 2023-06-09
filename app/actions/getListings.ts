4adff3b02f465a13603d3523f57f0b1ff9d991ca
Finn Huynh <trachuynh2014@gmail.com>
2023-06-09 18:09:15 -0400

    finish adding logic for search modal, add error and loading state

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -2,15 +2,79 @@ import prisma from "@/app/libs/prismadb";
 
 export interface IListingsParams {
   userId?: string;
+  guestCount?: number;
+  roomCount?: number;
+  bathroomCount?: number;
+  startDate?: string;
+  endDate?: string;
+  locationValue?: string;
+  category?: string;
 }
 
 export default async function getListings(params: IListingsParams) {
   try {
-    const { userId } = params;
+    const {
+      userId,
+      roomCount,
+      bathroomCount,
+      guestCount,
+      startDate,
+      endDate,
+      locationValue,
+      category,
+    } = params;
     let query: any = {};
     if (userId) {
       query.userId = userId;
     }
+    if (category) {
+      query.category = category;
+    }
+
+    if (roomCount) {
+      query.roomCount = {
+        gte: +roomCount,
+      };
+    }
+
+    if (bathroomCount) {
+      query.bathroomCount = {
+        gte: +bathroomCount,
+      };
+    }
+
+    if (guestCount) {
+      query.guestCount = {
+        gte: +guestCount,
+      };
+    }
+    if (locationValue) {
+      query.locationValue = locationValue;
+    }
+
+    // Check if both startDate and endDate exist
+    if (startDate && endDate) {
+      // Add a NOT filter to exclude listings with overlapping reservations
+      query.NOT = {
+        reservations: {
+          some: {
+            OR: [
+              // Check if reservation spans the selected date range
+              {
+                endDate: { gte: startDate },
+                startDate: { lte: endDate },
+              },
+              // Check if reservation starts before the selected end date but ends within or after it
+              {
+                startDate: { lte: endDate },
+                endDate: { gte: endDate },
+              },
+            ],
+          },
+        },
+      };
+    }
+
     const listings = await prisma.listing.findMany({
       where: query,
       orderBy: {
